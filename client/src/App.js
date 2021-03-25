7da3cb0e45dd29a82a144bfa2629d861d2cc8418
unknown <vstzen@gmail.com>
2021-03-24 20:18:58 -0700

    Fixed routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import LoginPage from "./Pages/LoginPage";
 import RegistrationPage from "./Pages/RegistrationPage";
-import RequestNewPage from "./Pages/RequestNewPage";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import RequestNewPage from './Pages/RequestNewPage';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
   export default function App() {
     return (
       <Router>
           <Switch>
-            <Route path="/">
-              <LoginPage />
-            </Route>
             <Route path="/register">
               <RegistrationPage />
+            </Route>
+            <Route exact path="/">
+              <LoginPage />
             </Route>
             <Route path="/request">
               <RequestNewPage />
