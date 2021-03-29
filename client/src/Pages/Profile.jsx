5dd34c947889544ea0ff11269572c212dce231d7
Tam Tran <ttt016@ucsd.edu>
2021-03-29 00:42:41 -0700

    update Profile page

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import pcimage from "../images/price-center.jpg"
 import avatar1 from "../images/avataaars-1.png"
-import avatar2 from "../images/avataaars-2.png"
-import avatar3 from "../images/avataaars-3.png"
-import avatar4 from "../images/anonymous-pic.png"
 
 import "./Profile.css";
 
@@ -18,6 +14,51 @@ class Profile extends Component{
                     <p id="ProfileTitle">
                         Profile Settings
                     </p>
+                </div>
+
+                <div class="ProfileWrapper">
+                    <div class="ProfileColumns">
+                        <div id="ProfileLeft">
+                            <div id="PFuserInputBackground"  className="PFInputBackground">
+                                <p id="PFuserLabel" className = "PFInputLabel">Name</p>
+                                <form>
+                                    <label>
+                                        <input type="text" autocomplete="off" id = "PFuserInput" />
+                                    </label>
+                                </form>
+                            </div>
+                            <div id="PFuserInputBackground"  className="PFInputBackground">
+                                <p id="PFuserLabel" className = "PFInputLabel">Email</p>
+                                <form>
+                                    <label>
+                                        <input type="text" autocomplete="off" id = "PFuserInput" />
+                                    </label>
+                                </form>
+                            </div>
+                            <div id="PFuserInputBackground"  className="PFInputBackground">
+                                <p id="PFuserLabel" className = "PFInputLabel">Password</p>
+                                <form>
+                                    <label>
+                                        <input type="text" autocomplete="off" id = "PFuserInput" />
+                                    </label>
+                                </form>
+                            </div>
+                            <div id="PFuserInputBackground"  className="PFInputBackground">
+                                <p id="PFuserLabel" className = "PFInputLabel">Major</p>
+                                <form>
+                                    <label>
+                                        <input type="text" autocomplete="off" id = "PFuserInput" />
+                                    </label>
+                                </form>
+                            </div>
+                            <div class="ProfileButtonCenter">
+                                <Button className="PFPageButtons" id="PF-save-button">SAVE</Button>
+                            </div>
+                        </div>
+                        <div id="ProfileRight">
+                            <img className="profile-avatar" src={avatar1} alt="profile-image"/>
+                        </div>
+                    </div>
                 </div>
                 
             </div>
